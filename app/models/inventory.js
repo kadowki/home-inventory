6d9856354dbc0f16b41a7e2d02482d0dd2a1da81
David B <daviddboling@gmail.com>
2014-07-23 10:12:28 -0500

    beginning of project

---
+++ app/models/inventory.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = Inventory;
---
+++ test/unit/inventoryTest.js
@@ -0,0 +1,6 @@
+/* globa describe, it */
+'use strict';
+
+
+var expect = require('chai').expect;
+var Inventory = require('../../app/models/inventory');
